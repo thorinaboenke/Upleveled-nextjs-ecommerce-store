d0524b16c93734e37cf27d71cdec3b3b3c0929a3
Thorina Boenke <thorina-boenke@web.de>
2020-10-02 18:15:57 +0200

    Start contact form

--- pages/checkout.js
+++ pages/checkout.js
@@ -11,6 +11,18 @@ export default function checkout() {
       </Head>
       <Layout>
         <h1>Checkout</h1>
+        <form>
+          <div>Contact Information</div>
+          <input placeholder="first name" type="text" required></input>
+          <input placeholder="last name" type="text" required></input>
+          <input placeholder="email" type="email" required></input>
+          <div>Shipping</div>
+          <input placeholder="Street" type="text" required></input>
+          <input placeholder="City" type="text" required></input>
+          <input placeholder="Country" type="text" required></input>
+          <div>Billing</div>
+          <input placeholder="Creditcard number" type="text" required></input>
+        </form>
       </Layout>
     </div>
   );
